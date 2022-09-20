e7e126f0b0fdad7c8969a594a00d10e8624d2ff8
abdullah-sameh <65913584+abdullah-sameh@users.noreply.github.com>
2022-09-20 16:10:04 +0200

    Update index.js
    edit for mobile users

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ const myInterval = setInterval(() => {
 window.onload = function(){
     let mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
     if (mobile) {
-        alert("Visit this on a Computer for Better View");              
+        if (confirm("Visit this on a Computer for Better View")) {
+            close();
+        }              
     }
 }
